6bdd9d91fa7eafb4002183a59a62face5b47000f
csyo25025-bit <csyo25025@g.nihon-u.ac.jp>
2026-07-22 19:24:14 +0900

    環境構築
    DX環境を使用できるように環境を構築した。

---
+++ コイントス/コイントス.cpp
@@ -0,0 +1,15 @@
+#include "DxLib.h"
+#include<random>
+
+using namespace std;
+
+random_device rd;
+mt19937 rand_mt(rd());
+uniform_int_distribution<> coin_reslt(0, 1);
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPREVInstance, LPSTR lpCmdLine, int nCmdShow) {
+
+	
+
+}
