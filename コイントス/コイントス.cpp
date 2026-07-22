a70f4a193cda4496775e1bf9662e859e619b5824
csyo25025-bit <csyo25025@g.nihon-u.ac.jp>
2026-07-22 19:32:04 +0900

    修正
    スペルミスの修正

--- コイントス/コイントス.cpp
+++ コイントス/コイントス.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 random_device rd;
 mt19937 rand_mt(rd());
-uniform_int_distribution<> coin_reslt(0, 1);
+uniform_int_distribution<> coin_result(0, 1);
 
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPREVInstance, LPSTR lpCmdLine, int nCmdShow) {
